bc4d1115ecf44cd4cf40103d561b6b0bf0bf2aa7
tallyhome <tally@obione.net>
2025-03-28 03:37:55 +0100

    correction de bug mineur

--- resources/views/layouts/partials/admin-navbar.blade.php
+++ resources/views/layouts/partials/admin-navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('admin.dashboard') }}">MyVcard MyPredict - Administration <span class="badge bg-danger">v{{ $appVersion }}</span></a>
+        <a class="navbar-brand" href="{{ route('home') }}">MyVcard MyPredict - Administration <span class="badge bg-danger">v{{ $appVersion }}</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
